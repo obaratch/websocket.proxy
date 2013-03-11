3f307ed7dd1f2e9e9fa2ef5278a7ede501ee7d06
Takashi Obara <takashi.obara@gmail.com>
2013-03-11 12:03:09 +0900

    404 handling

--- HttpServer.js
+++ HttpServer.js
@@ -18,15 +18,24 @@ module.exports = function(port, handlers){
 
 	server
 		.use(connect.bodyParser())
-		.use(function(req, res){
+		.use(function(req, res, next){
 			var method = methods[req.method];
 			if(!method){
 				logger.error("unsupprted method:" + req.method);
-				res.statusCode=500;
+				res.statusCode = 500;
 				res.end();
 			} else {
-				method(req, res);
+				method(req, res, next);
 			}
+		})
+		.use(function(req, res){
+			logger.error("never handled. url=" + req.url);
+			// logger.error(req);
+			// logger.error(res);
+			var msg = "404 not found: " + req.url;
+			res.statusCode = 404;
+			res.setHeader('Content-Length', msg.length);
+			res.end(msg);
 		});
 
 	server.listen(port, function(){
@@ -37,18 +46,18 @@ module.exports = function(port, handlers){
 	return server;
 };
 
-function doGet(req, res){
+function doGet(req, res, next){
 	logger.trace('HTTP GET', req.url);
 	if(req.url=='/list'){
 		var handler = _handlers["LIST"];
-		if(handler) handler(req, res);
+		if(handler) handler(req, res, next);
 		res.end();
 	} else {
-		_staticHandler(req, res);
+		_staticHandler(req, res, next);
 	}
 }
 
-function doPost(req, res){
+function doPost(req, res, next){
 	logger.trace('HTTP POST', req.url, req.body);
 	var handler = _handlers["POST"];
 	if(handler) handler(req, res, req.body);
